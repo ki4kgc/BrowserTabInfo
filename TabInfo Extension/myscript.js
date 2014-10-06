c7a83012f90144a6109f23b65bd361700449d826
ki4kgc <jburns@gwmail.gwu.edu>
2014-10-06 15:00:33 -0400

    Extension displays id and url of all open tabs
    also displays num of windows and num of tabs on each window.  does not
    use new api

--- TabInfo Extension/myscript.js
+++ TabInfo Extension/myscript.js
@@ -1,3 +1,16 @@
+function getTabInfo () {
+	// This fetches the information from chrome API
+
+	var url;
+	var id 
+
+	chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
+    url = tabs[0].url;
+    id = tabs[0].index;
+    document.getElementById("infoField").innerHTML = 'The url for this tab is: ' + url + ' and the id is: ' + id; 
+	});
+}
+
 function displayInfo() {
 	
 	var output = '';
@@ -20,7 +33,53 @@ function displayInfo() {
 	
 }
 
+// Code adapted from chromium merge windows example 
+// http://src.chromium.org/viewvc/chrome/trunk/src/chrome/common/extensions/docs/examples/api/windows/merge_windows/
+var output = '';
+var targetWindow = null;
+var tabCount = 0;
+
+function start(tab) {
+  chrome.windows.getCurrent(getWindows);
+}
+
+function getWindows(win) {
+  targetWindow = win;
+  chrome.tabs.getAllInWindow(targetWindow.id, getTabs);
+}
+
+function getTabs(tabs) {
+  tabCount = tabs.length;
+  // We require all the tab information to be populated.
+  chrome.windows.getAll({"populate" : true}, moveTabs);
+}
+
+function moveTabs(windows) {
+  var numWindows = windows.length;
+  var tabPosition = tabCount;
+  var url = '';
+
+  output += '<b>You have ' + numWindows + ' windows open</b><br>***********************************<br>';
+  for (var i = 0; i < numWindows; i++) {
+    var win = windows[i];
+	var numTabs = win.tabs.length;
+	output += '<b>There are ' + numTabs + ' tabs in window ' + i + '</b><br>___________________________<br>';
+
+	for (var j = 0; j < numTabs; j++) {
+		var tab = win.tabs[j];
+		var url = tab.url;
+		// Move the tab into the window that triggered the browser action.
+		output += ' -> The url of tab ' + j + ' is ' + url + '<br>';
+		//chrome.tabs.move(tab.id,{"windowId": targetWindow.id, "index": tabPosition});
+		tabPosition++;
+	}
+  }
+  document.getElementById("infoField").innerHTML = output; 
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-	displayInfo();
+	//displayInfo();
+	//getTabInfo();
+	start();
 
 });
